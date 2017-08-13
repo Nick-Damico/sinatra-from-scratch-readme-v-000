5d3c9a97f7415af059f4ecfab5eca34168fa19c6
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-13 14:32:16 +0000

    initial upload of first Sinatra App.

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+
+# The Controller of our App. handles all incoming 'requests',
+# And returns or sends back the appropriate responses to the client.
+
+# This line allows us to inherit from Sinatra Class.
 class App < Sinatra::Base
 
-  get '/' do 
-    "Hello, world!"
+  get '/' do
+    "This is your first Sinatra App!"
   end
 
 end
